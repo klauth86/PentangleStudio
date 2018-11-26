136200d41708284223974df9e74c8bfd18d77492
Artur Khayrullin <kehel@yandex.ru>
2018-11-26 19:49:02 +0300

    Add primitives

---
+++ BridgeMaster/Assets/Scripts/Pickups/PickupableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PickupableObject : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Player>().AddPickup(this);
+        Destroy(gameObject);
+    }
+}
---
+++ BridgeMaster/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField]
+    private float _velocity;
+
+    private List<PickupableObject> _pickUps;
+
+    internal void AddPickup(PickupableObject pickupableObject)
+    {
+        _pickUps.Add(pickupableObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        _pickUps = new List<PickupableObject>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        var k = 0;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            k = 1;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            k = -1;
+        transform.position = new Vector2(transform.position.x + _velocity * k * Time.deltaTime, transform.position.y);
+    }
+}
