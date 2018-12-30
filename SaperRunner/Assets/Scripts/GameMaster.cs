8f9d8a095753286c5178dce2e27a21dec46d7a37
Artur Khayrullin <kehel@yandex.ru>
2018-12-30 18:56:45 +0300

    Add SaperRunner proj

---
+++ SaperRunner/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+    
+    public static GameMaster Instance { get; set; }
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+    public void NewGame() {
+
+    }
+}
---
+++ SaperRunner/Assets/Scripts/Interface/IGameBoard.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public interface IGameBoard {
+    int Dimension { get; }
+    ICard[] Cards { get; }
+
+    IEnumerable<ICard> GetNeighbours(ICard card);
+}
