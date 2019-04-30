bfebeeeb0bd7da3772b9e11773eb4e3753a4b9be
Artur Khayrullin <kehel@yandex.ru>
2019-04-30 13:34:18 +0300

    Refresh sprites and add Attack anim and Death anim

--- BridgeMaster_v2/Assets/Scripts/Player/Player.cs
+++ BridgeMaster_v2/Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour {
     [SerializeField] private float _velocity;
     [SerializeField] private float _jump;
@@ -9,18 +10,20 @@ public class Player : MonoBehaviour {
     private bool _isJumping;
     private Rigidbody2D _rigidbody;
     private Animator _animator;
+    private Collider2D _collider;
 
     // Use this for initialization
     void Start() {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _collider = GetComponentInChildren<Collider2D>();
     }
 
     // Update is called once per frame
     void Update() {
         Run();
         Jump();
-        CastSpell();
+        Attack();
     }
 
     void Run() {
@@ -37,19 +40,26 @@ public class Player : MonoBehaviour {
     }
 
     void Jump() {
-        if (Input.GetButtonDown("Fire1")) {
-            if (!_isJumping) {
-                _isJumping = true;
-                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jump);
-                _animator.SetBool("IsJumping", true);
-            }
-        }
-        if (_isJumping && Mathf.Abs(_rigidbody.velocity.y) < float.Epsilon) {
-            _isJumping = false;
-            _animator.SetBool("IsJumping", false);
+        if (Input.GetButtonDown("Fire1") && !_isJumping) {
+            _isJumping = true;
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jump);
+            _animator.SetBool("IsJumping", true);
         }
     }
 
-    void CastSpell() {
+    void Attack() {
+        if (Input.GetButtonDown("Fire2")) {
+            _animator.SetBool("IsAttacking", true);
+        }
+        else if (Input.GetButtonUp("Fire2")) {
+            _animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (_isJumping) {
+            _isJumping = false;
+            _animator.SetBool("IsJumping", false);
+        }
     }
 }
