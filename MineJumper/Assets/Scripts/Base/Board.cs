cac6a96d34cfb203ea39dd8f1e09d697710502b3
Artur Khayrullin <kehel@yandex.ru>
2019-05-23 14:34:12 +0300

    Add MineJumper proj

---
+++ MineJumper/Assets/Scripts/Base/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Base {
+    public class Board {
+        public int Dim;
+        public int Size;
+
+        public int BoardSize { get { return (int)Math.Pow(Size, Dim); } }
+
+        public Card[] Cards;
+
+        public Board(int dim, int size, int bombs) {
+            Cards = new Card[BoardSize];
+
+            for (int i = 0; i < BoardSize; i++) {
+                var card = new Card();
+                if (i < bombs)
+                    card.HasBomb = true;
+            }
+
+            Shuffle();
+        }
+
+        private void Shuffle() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MineJumper/Assets/Scripts/Base/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Base {
+    public class Card {
+        public bool IsMarked;
+        public bool IsRevealed;
+        public bool HasBomb;
+        public int BombIndex;
+
+        public event Action OnReveal = delegate { };
+
+        public void Reveal() {
+            OnReveal();
+        }
+
+        public event Action OnMark = delegate { };
+
+        public void Mark() {
+            OnMark();
+        }
+    }
+}
