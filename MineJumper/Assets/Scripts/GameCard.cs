31388b9ab4adbddff1df8a2af37de30778e8b81d
Artur Khayrullin <kehel@yandex.ru>
2019-05-23 15:42:19 +0300

    MineJumper p4

--- MineJumper/Assets/Scripts/GameCard.cs
+++ MineJumper/Assets/Scripts/GameCard.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class GameCard : MonoBehaviour {
-
-    [SerializeField] private Material[] _materials;
+    [SerializeField] private Material _unrevealedMaterial;
     [SerializeField] private Material _markedMaterial;
+
+    [SerializeField] private Material[] _indexMaterials;
 
     private MeshRenderer _meshRenderer;
     private MeshRenderer MeshRenderer {
@@ -15,7 +16,6 @@ public class GameCard : MonoBehaviour {
     }
 
     private Card card;
-
     public Card Card {
         get {
             return card;
@@ -32,7 +32,7 @@ public class GameCard : MonoBehaviour {
         if (card == null)
             return;
 
-        MeshRenderer.material = _materials[card.BombIndex];
+        MeshRenderer.material = _indexMaterials[card.BombIndex];
 
         card.OnMark += OnMark;
         card.OnReveal += OnReveal;
@@ -47,10 +47,10 @@ public class GameCard : MonoBehaviour {
     }
 
     private void OnMark(Card card) {
-        MeshRenderer.material = card.IsMarked ? _markedMaterial : _materials[card.BombIndex];
+        MeshRenderer.material = card.IsMarked ? _markedMaterial : (card.IsRevealed ? _indexMaterials[card.BombIndex] : _unrevealedMaterial);
     }
 
     private void OnReveal(Card card) {
-
+        MeshRenderer.material = _indexMaterials[card.BombIndex];
     }
 }
