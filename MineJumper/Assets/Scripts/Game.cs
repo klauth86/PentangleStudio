1fbd766c00292a40d23c3cfb12073bad39a0afee
Artur Khayrullin <kehel@yandex.ru>
2019-05-24 16:04:11 +0300

    MineJumper p9

--- MineJumper/Assets/Scripts/Game.cs
+++ MineJumper/Assets/Scripts/Game.cs
@@ -7,6 +7,7 @@ public class Game : MonoBehaviour {
 
     [SerializeField] private int _size;
     [SerializeField] private int _bombs;
+    [SerializeField] private float _scaleFactor;
 
     // Use this for initialization
     void Start() {
@@ -18,13 +19,15 @@ public class Game : MonoBehaviour {
     void CreateBoard(Board board) {
         var offset = board.Size % 2 == 0 ? 0.5f : 0.0f;
         for (int i = 0; i < board.BoardSize; i++) {
-            var gameCard = Instantiate(_gameCardPrefab, new Vector3(i % board.Size - board.Size / 2 + offset, 0, i / board.Size - board.Size / 2 + offset), Quaternion.identity, transform).GetComponent<GameCard>();
+            var gameCard = Instantiate(_gameCardPrefab,
+                new Vector3(_scaleFactor * (i % board.Size - board.Size / 2 + offset), 0,
+                _scaleFactor * (i / board.Size - board.Size / 2 + offset)), Quaternion.identity, transform).GetComponent<GameCard>();
             gameCard.Card = board.Cards[i];
         }
     }
 
     private void AdjustCamera(Board board) {
-        var offset = (board.Size / 2 + 1) / Mathf.Tan(Mathf.PI / 6);
+        var offset = _scaleFactor * board.Size / 2 * (1 + Mathf.Tan(Mathf.PI / 12)) + 0.5f;
         Camera.main.transform.position = new Vector3(0, offset, -offset);
     }
 }
---
+++ MineJumper/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float _minY;
+    [SerializeField] private float _deltaY;
+
+    private Rigidbody _rigidbody;
+    private Rigidbody Rigidbody {
+        get {
+            return _rigidbody ?? (_rigidbody = GetComponent<Rigidbody>());
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (transform.position.y < _minY + _deltaY)
+            Rigidbody.velocity =  - Rigidbody.velocity;
+
+    }
+}
