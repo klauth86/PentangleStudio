9f628969687ddd97810c92769d99f6c7ff775ba5
Artur Khayrullin <kehel@yandex.ru>
2019-05-26 20:42:24 +0300

    Remove old proj, add new proj for VFX experiments

--- SaperRunner/Assets/Scripts/Interface/IGameBoard.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-public interface IGameBoard {
-    int Dimension { get; set; }
-    ICard[] Cards { get; set; }
-    IEnumerable<ICard> GetNeighbours(ICard card);
-}
