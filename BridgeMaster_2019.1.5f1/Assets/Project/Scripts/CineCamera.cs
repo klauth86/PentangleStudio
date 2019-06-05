faddec685dd6704813b47db3b64dc17be9a6a6e0
Artur Khayrullin <kehel@yandex.ru>
2019-06-05 17:58:58 +0300

    BridgeMaster 2019 p1

---
+++ BridgeMaster_2019.1.5f1/Assets/Project/Scripts/CineCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CineCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    [SerializeField] private float _vertOffset;
+    [SerializeField] private float _horOffset;
+
+    [SerializeField] private float _leftEdge;
+    [SerializeField] private float _upEdge;
+    [SerializeField] private float _rightEdge;
+    [SerializeField] private float _downEdge;
+
+    private Transform _transform;
+
+    private void Start() {
+        if (_target) {
+            transform.position = _target.position;
+        }
+        else {
+            Debug.LogWarning("_transform is not init in Inspector!");
+        }
+        _transform = transform;
+    }
+
+    private void FixedUpdate() {
+    }
+}
