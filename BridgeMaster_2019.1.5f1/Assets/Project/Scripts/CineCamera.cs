4f9a1140348e68a6f6ca4551016bad80caf72748
Artur Khayrullin <kehel@yandex.ru>
2019-06-10 23:52:26 +0300

    BridgeMaster 2019 p14

--- BridgeMaster_2019.1.5f1/Assets/Project/Scripts/CineCamera.cs
+++ BridgeMaster_2019.1.5f1/Assets/Project/Scripts/CineCamera.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CineCamera : MonoBehaviour {
     [SerializeField] private Transform _target;
-    [SerializeField] private float _cameraFollowDuration;
+    [SerializeField] private float _cameraFollowTimeoutDuration;
+    [SerializeField] private float _cameraFollowLerpDuration;
 
     [SerializeField] private float _vertOffsetInPercent;
     [SerializeField] private float _horOffsetInPercent;
 
     [SerializeField] private float _leftEdge;
-    [SerializeField] private float _upEdge;
     [SerializeField] private float _rightEdge;
+    [SerializeField] private float _upEdge;
     [SerializeField] private float _downEdge;
 
     private Transform _transform;
@@ -19,7 +20,9 @@ public class CineCamera : MonoBehaviour {
 
     private void Awake() {
         if (_target) {
-            transform.position = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+            transform.position = new Vector3(
+                Mathf.Clamp(_target.position.x, _leftEdge, _rightEdge),
+                Mathf.Clamp(_target.position.y, _downEdge, _upEdge), transform.position.z);
         }
         else {
             Debug.LogWarning("_transform is not init in Inspector!");
@@ -47,8 +50,15 @@ public class CineCamera : MonoBehaviour {
             if (Mathf.Abs(_target.position.y - camy) > _vertOffsetInPercent * _camera.orthographicSize) {
                 camy = _target.position.y;
             }
-            _transform.position = Vector3.Lerp(_transform.position, new Vector3(camx, camy, _transform.position.z), _cameraFollowDuration);
-            yield return new WaitForSeconds(_cameraFollowDuration);
+            _transform.position = Vector3.Lerp(_transform.position, new Vector3(
+                _leftEdge < _rightEdge
+                    ? Mathf.Clamp(camx, _leftEdge + _camera.orthographicSize * _camera.aspect, _rightEdge - _camera.orthographicSize * _camera.aspect)
+                    : _leftEdge,
+                _downEdge < _upEdge 
+                    ? Mathf.Clamp(camy, _downEdge + _camera.orthographicSize, _upEdge - _camera.orthographicSize)
+                    : _downEdge,
+                _transform.position.z), _cameraFollowLerpDuration);
+            yield return new WaitForSeconds(_cameraFollowTimeoutDuration);
         }
     }
 }
