807c63e7c727ddc595f6c6ff8206a244bfc26372
Artur Khayrullin <kehel@yandex.ru>
2019-06-10 23:00:40 +0300

    BridgeMaster 2019 p13

--- BridgeMaster_2019.1.5f1/Assets/Project/Scripts/CineCamera.cs
+++ BridgeMaster_2019.1.5f1/Assets/Project/Scripts/CineCamera.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-public class CineCamera : MonoBehaviour
-{
+[RequireComponent(typeof(Camera))]
+public class CineCamera : MonoBehaviour {
     [SerializeField] private Transform _target;
+    [SerializeField] private float _cameraFollowDuration;
 
-    [SerializeField] private float _vertOffset;
-    [SerializeField] private float _horOffset;
+    [SerializeField] private float _vertOffsetInPercent;
+    [SerializeField] private float _horOffsetInPercent;
 
     [SerializeField] private float _leftEdge;
     [SerializeField] private float _upEdge;
@@ -13,17 +15,40 @@ public class CineCamera : MonoBehaviour
     [SerializeField] private float _downEdge;
 
     private Transform _transform;
+    private Camera _camera;
 
-    private void Start() {
+    private void Awake() {
         if (_target) {
-            transform.position = _target.position;
+            transform.position = new Vector3(_target.position.x, _target.position.y, transform.position.z);
         }
         else {
             Debug.LogWarning("_transform is not init in Inspector!");
         }
         _transform = transform;
+        _camera = GetComponent<Camera>();
     }
 
-    private void FixedUpdate() {
+    private void OnEnable() {
+        StartCoroutine(FollowRoutine());
+    }
+
+    private void OnDisable() {
+        StopAllCoroutines();
+    }
+
+    IEnumerator FollowRoutine() {
+        while (true) {
+            var camx = _transform.position.x;
+            var camy = _transform.position.y;
+
+            if (Mathf.Abs(_target.position.x - camx) > _horOffsetInPercent * _camera.orthographicSize * _camera.aspect) {
+                camx = _target.position.x;
+            }
+            if (Mathf.Abs(_target.position.y - camy) > _vertOffsetInPercent * _camera.orthographicSize) {
+                camy = _target.position.y;
+            }
+            _transform.position = Vector3.Lerp(_transform.position, new Vector3(camx, camy, _transform.position.z), _cameraFollowDuration);
+            yield return new WaitForSeconds(_cameraFollowDuration);
+        }
     }
 }
