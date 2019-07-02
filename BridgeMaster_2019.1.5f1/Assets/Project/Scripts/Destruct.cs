49449d0ec9e3ffb598e1b05aa96f39b4f40fe195
Artur Khayrullin <kehel@yandex.ru>
2019-07-02 23:15:27 +0300

    BridgeMaster 2019 p41

---
+++ BridgeMaster_2019.1.5f1/Assets/Project/Scripts/Destruct.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Destruct : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D[] _rbs;
+    [SerializeField] private bool _destruct;
+    [SerializeField] private float _destructionRate=0.1f;
+
+    private void Update() {
+        if (_destruct && _rbs != null) {
+            _destruct = false;
+            StartCoroutine(DestructRoutine(Random.Range(0, _rbs.Length)));
+        }
+    }
+
+    private IEnumerator DestructRoutine(int startIndex) {
+        int i = 1;
+        _rbs[startIndex].isKinematic = false;
+
+        while (i < _rbs.Length) {
+            yield return new WaitForSeconds(_destructionRate);
+            if (startIndex - i >= 0)
+                _rbs[startIndex - i].isKinematic = false;
+
+            if (startIndex + i < _rbs.Length)
+                _rbs[startIndex + i].isKinematic = false;
+
+            i++;
+        }
+    }
+}
